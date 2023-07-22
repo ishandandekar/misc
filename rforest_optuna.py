387d2e6db93ea51b888e109b399030032d8e5eda
Ishan Dandekar <ishandandekar2002@gmail.com>
2023-07-22 22:25:34 +0530

    Update optuna scripts

--- rforest_optuna.py
+++ rforest_optuna.py
@@ -2,7 +2,7 @@ import optuna
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
@@ -13,7 +13,7 @@ def tune_random_forest_classifier(features, target):
         features, target, test_size=0.2, random_state=42
     )
 
-    def objective(trial):
+    def objective(trial: optuna.Trial):
         params = {
             "n_estimators": trial.suggest_int("n_estimators", 100, 1000, step=100),
             "max_depth": trial.suggest_int("max_depth", 3, 10),
@@ -30,7 +30,7 @@ def tune_random_forest_classifier(features, target):
         model.fit(X_train, y_train)
 
         y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
+        accuracy = roc_auc_score(y_test, y_pred)
         return accuracy
 
     study = optuna.create_study(direction="maximize")
--- xgboost_optuna.py
+++ xgboost_optuna.py
@@ -2,20 +2,15 @@ import optuna
 import xgboost as xgb
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
 import os
 
 
-def tune_xgboost_classifier(features, target):
-    X_train, X_test, y_train, y_test = train_test_split(
-        features, target, test_size=0.2, random_state=42
-    )
-
+def tune_xgboost_classifier(X_train, X_test, y_train, y_test):
     def objective(trial):
         params = {
-            "objective": "multi:softmax",
-            "eval_metric": "mlogloss",
-            "num_class": len(set(target)),
+            "objective": "binary:logistic",
+            "eval_metric": "auc",
             "eta": trial.suggest_float("eta", 0.01, 0.3, log=True),
             "max_depth": trial.suggest_int("max_depth", 3, 10),
             "subsample": trial.suggest_float("subsample", 0.5, 1.0),
@@ -23,6 +18,7 @@ def tune_xgboost_classifier(features, target):
             "lambda": trial.suggest_float("lambda", 0.01, 10.0, log=True),
             "alpha": trial.suggest_float("alpha", 0.01, 10.0, log=True),
             "min_child_weight": trial.suggest_int("min_child_weight", 1, 10),
+            "early_stopping_rounds": 10,
             "random_state": 42,
         }
 
@@ -32,18 +28,15 @@ def tune_xgboost_classifier(features, target):
             X_train,
             y_train,
             eval_set=[(X_test, y_test)],
-            early_stopping_rounds=10,
             verbose=False,
             callbacks=[
-                optuna.integration.XGBoostPruningCallback(
-                    trial, "validation_0-mlogloss"
-                )
+                optuna.integration.XGBoostPruningCallback(trial, "validation_0-auc")
             ],
         )
 
         y_pred = model.predict(X_test)
-        accuracy = accuracy_score(y_test, y_pred)
-        return accuracy
+        fscore = f1_score(y_test, y_pred)
+        return fscore
 
     study = optuna.create_study(direction="maximize")
     study.optimize(objective, n_trials=100)
@@ -51,16 +44,28 @@ def tune_xgboost_classifier(features, target):
     best_params = study.best_params
     best_model = xgb.XGBClassifier(**best_params)
 
-    best_model.fit(features, target)
+    best_model.fit(X_train, y_train)
 
     # Create 'figures' directory if it doesn't exist
     os.makedirs("figures", exist_ok=True)
 
     # Visualizations
     param_importances = optuna.visualization.plot_param_importances(study)
-    param_importances.write_image("figures/param_importances.png")
+    param_importances.to_html("param_importances.html")
 
     parallel_coordinate = optuna.visualization.plot_parallel_coordinate(study)
-    parallel_coordinate.write_image("figures/parallel_coordinate.png")
+    parallel_coordinate.to_html("parallel_coordinate.png")
 
     return best_model, best_params
+
+
+X, y = load_iris(return_X_y=True, as_frame=True)
+X, y = X[:100], y[:100]
+print(y.value_counts())
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+best_model, best_params = tune_xgboost_classifier(X_train, X_test, y_train, y_test)
+print("\n\n")
+print("#" * 10, end="\n\n")
+print(best_params)
