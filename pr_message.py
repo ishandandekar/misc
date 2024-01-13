1bf1b362d54a37a40fa4d73e4e9b5ec0caed5db9
ishandandekar <ishandandekar2002@gmail.com>
2024-01-13 23:48:30 +0530

    fix name

--- pr_message.py
+++ pr_message.py
@@ -4,7 +4,7 @@ from csv2md.table import Table
 df = pd.DataFrame({"a": [1, 2, 3], "b": ["hi", "hello", "hey"]})
 df.to_csv("sample.csv")
 
-with open("input.csv") as f:
+with open("sample.csv") as f:
     table = Table.parse_csv(f)
 
 print(table.markdown())
